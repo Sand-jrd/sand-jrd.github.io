6a2876c020691608f5baadd63628a277e5e901fa
Sandrine Juillard <47563242+Sand-jrd@users.noreply.github.com>
2023-07-12 09:06:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
 // -------- List of publication --------
 
-// So I have problem with core policy ...
-$.getJSON("publication.json", function(json) {
-    document.getElementById("ADS").textContent = json;
-})
+
+query = "q=author%3AJuillard%2C+Sandrine&fl=title%2C+author%2C+bibcode%2C+year&rows=6";
 
 $(document).ready(function() {
     $.ajax({
-        url: 'https://api.adsabs.harvard.edu/v1/search/query?q=star&fl=bibcode',
+        url: 'https://corsproxy.io/?https://api.adsabs.harvard.edu/v1/search/query?'+query,
         beforeSend: function(xhr) {
              xhr.setRequestHeader("Authorization","Bearer MPO6caNhGOjkCeT7ebkh8GyksYnbOeDfzb0hpiZk");
         }, 
+        dataType: "text",       
         headers: {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS', 'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'},
         success: function(data){
-            alert(data);
-            document.getElementById("ADS").textContent = data;
+            
+            var json = JSON.parse(data)['response']['docs'];
+            var info_type = ["title", "year", "author"];
+            for (var i=0;i<json.length;++i)
+            {
+                var publication_i = document.createElement("li");
+                for (var k=0;k<info_type.length;++k){
+                
+                    // Content from JSON
+                    var info = info_type[k]
+                    var content = json[i][info]
+                    
+                    if (info == "title"){
+                        content = content[0];
+                        var adress = "https://ui.adsabs.harvard.edu/abs/"+json[i]['bibcode']+"/abstract";
+                    }
+                    if (info == "author"){
+                        var tmp_content = content.splice(0, 3);
+                        if(content.length > 3){
+                            tmp_content.push("et al.");
+                        }
+                        tmp_content.join(',    ');
+                        content = tmp_content;
+                    }
+                    
+                    // Create div
+                    if (info == "title"){
+                        var ele = document.createElement("a");
+                        ele.setAttribute("href", adress);
+                    }else {
+                        var ele = document.createElement("div");
+                    }
+                    ele.classList.add(info);
+                    ele.appendChild(document.createTextNode(content));
+                    publication_i.appendChild(ele);
+                      
+                    document.getElementById("ADS").appendChild(publication_i);
+                    }
+            }
+
         },
     })
 });
